009e021be11b4c30ac1c8fc53fa15aa18f5f0e76
sahyunjin <tkguswls1106@naver.com>
2023-05-01 14:15:59 +0900

    초기설정 및 틀 개발 완료. 데이터베이스 개발 전 상태임.

---
+++ src/main/java/com/umsinsa/solvingproblemspringproject/SolvingproblemSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.umsinsa.solvingproblemspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingproblemSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvingproblemSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umsinsa/solvingproblemspringproject/SolvingproblemSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umsinsa.solvingproblemspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvingproblemSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
